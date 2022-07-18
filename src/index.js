1f9e047ff1adb9da242aebff9fcae2808726e7a3
Vlad <vlad.mingaleev01@mail.ru>
2022-07-18 11:50:56 +0300

    feat: 20tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+		const config = bracketsConfig.map((value) => value.join(""));
+		console.log(str);
+		let i = -1;
+		while (i < config.length) {
+			i++;
+			if (str.indexOf(config[i]) !== -1) {
+				str =
+					str.slice(0, str.indexOf(config[i])) +
+					str.slice(str.indexOf(config[i]) + 2, str.length);
+				i = -1;
+			}
+		}
+		return str.length === 0;
+} 
+
